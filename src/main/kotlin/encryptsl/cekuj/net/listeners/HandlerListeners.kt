e1d92f8ab039367f3ce2f4a8f6c9903228c5722a
EncryptSL <rydlos123@gmail.com>
2022-03-06 15:42:15 +0100

    Fix missing registering for listeners

--- src/main/kotlin/encryptsl/cekuj/net/listeners/HandlerListeners.kt
+++ src/main/kotlin/encryptsl/cekuj/net/listeners/HandlerListeners.kt
@@ -8,7 +8,10 @@ class HandlerListeners(private val countryBlocker: CountryBlocker) {
     fun registerListeners() {
         val start = System.currentTimeMillis()
         val list: List<Listener> = arrayListOf(PlayerLoginEventClass(countryBlocker))
-        list.forEach { s ->  countryBlocker.slF4JLogger.info("Bukkit listeners ${s.javaClass.simpleName} registered () -> ok") }
+        list.forEach { s ->
+            countryBlocker.server.pluginManager.registerEvents(s, countryBlocker)
+            countryBlocker.slF4JLogger.info("Bukkit listeners ${s.javaClass.simpleName} registered () -> ok")
+        }
         countryBlocker.slF4JLogger.info("Listeners registered(${list.size}) in time ${System.currentTimeMillis() - start} ms -> ok")
     }
 
