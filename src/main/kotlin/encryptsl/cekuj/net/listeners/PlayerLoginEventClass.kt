4d193ed5c1dec0d776ea202846a40be6df3a19f1
EncryptSL <rydlos123@gmail.com>
2022-03-06 16:52:55 +0100

    Improvements of code with MiniMessage

--- src/main/kotlin/encryptsl/cekuj/net/listeners/PlayerLoginEventClass.kt
+++ src/main/kotlin/encryptsl/cekuj/net/listeners/PlayerLoginEventClass.kt
@@ -1,26 +1,37 @@
 package encryptsl.cekuj.net.listeners
 
 import encryptsl.cekuj.net.CountryBlocker
-import net.kyori.adventure.text.Component
+import encryptsl.cekuj.net.util.IPTool
+import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerLoginEvent
+import java.net.InetAddress
 
 class PlayerLoginEventClass(private val countryBlocker: CountryBlocker) : Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     fun detectedCountry(event: PlayerLoginEvent) {
-        val country = countryBlocker.database.getDatabase().country(event.player.address.address).country.isoCode
-        if (event.player.address.address.hostAddress == "127.0.0.1") return
+        if (IPTool.getIP(event.player) == "127.0.0.1") return
+        val country = countryBlocker.database.getDatabase().country(InetAddress.getByName(IPTool.getIP(event.player))).country.isoCode
         countryBlocker.config.getStringList("CountryBlocker.blockList").forEach { country_code ->
             if (country == country_code) {
+                Bukkit.broadcast(MiniMessage.miniMessage().deserialize(
+                    countryBlocker.config.getString("CountryBlocker.announcement").toString(),
+                    TagResolver.resolver(
+                        Placeholder.parsed("user", event.player.name),
+                        Placeholder.parsed("ip", IPTool.getIP(event.player)),
+                        Placeholder.parsed("country", country),
+                        Placeholder.parsed("time", IPTool.getTime())
+                    )
+                ), "country.blocker.announcement")
                 event.disallow(
                     PlayerLoginEvent.Result.KICK_OTHER,
-                    Component.text(
-                        countryBlocker.config.getString("CountryBlocker.deny_message")!!
-                            .replace("[connected_country]", country)
-                    )
+                    MiniMessage.miniMessage().deserialize(countryBlocker.config.getString("CountryBlocker.deny_message").toString(), TagResolver.resolver(Placeholder.parsed("connected_country", country)))
                 )
             }
         }
